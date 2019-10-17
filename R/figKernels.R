dad607579bc2f685828c0cbf36f700cbe5325c35
Kevin Cazelles <kcazelle@uoguelph.ca>
2019-10-17 17:14:31 -0400

    review code for figure 5 :bar_chart:

--- R/figKernels.R
+++ R/figKernels.R
@@ -18,10 +18,12 @@
 #' @export
 #'
 #' @examples
-#' figKernels(res_bestModels)
+#'
+
+
 
 figKernels <- function(datares, age = 1, xlim = c(0, 50), ylim = c(0, 0.015),
-  colors = c("#5d393f", "#4899c7", "#c8de86", "grey10")) {
+  colors = c("#6b2934", "#4899c7", "#c8de86", "grey10")) {
     ##
     nm_tre <- as.character(unique(datares$tree))
     ## Colors
@@ -29,56 +31,67 @@ figKernels <- function(datares, age = 1, xlim = c(0, 50), ylim = c(0, 0.015),
     ## Line types
     lty <- rep(c(1, 2), each = 4, length.out = length(nm_tre))
     ## text size
-    cex.txt <- 2
+    cex.txt <- 1.1
     ## Layout matrix
-    matlay <- cbind(7, rbind(8, matrix(1:6, 2), 9))
-    matlay[c(1, 4), 1] <- 0
+    matlay <- cbind(7, rbind(matrix(1:6, 2), 8))
+    matlay[3, 1] <- 0
     ##
     seqd <- seq(xlim[1], xlim[2], .01)
     ##
     # datares <- datares[datares$year == year, ]
-    datares <- datares[datares$age == 1, ]
+    datares <- datares[datares$age == age, ]
     # datares$tosplit <- paste0(datares$site, datares$year)
     datares$tosplit <- paste0(datares$site, datares$year)
     ls_tre <- split(datares, datares$tosplit)
 
+    regions <- c("Aitibi", "", "Bic",  "", "Sutton", "")
     ##
-    layout(mat = matlay, widths = c(0.2, 1, 1, 1), heights = c(0.15, 1, 1, 0.5))
-    par(mar = c(1.5, 2.25, 0.5, 1.25), las = 1, mgp = c(2.2, 0.75, 0))
+    layout(mat = matlay, widths = c(0.3, 1, 1, 1), heights = c(1, 1, 0.4))
+    par(mar = c(1.2, 2, 0.5, .8), las = 1, cex.axis = .6, lend = 1, tck = -0.05)
     ##
     for (i in seq_along(ls_tre)) {
-        plot0(xlim, ylim, xpd = FALSE)
-        axis(1, lwd = 0, lwd.ticks = .5)
-        axis(2, lwd = 0, lwd.ticks = .5)
-        for (j in 1:nrow(ls_tre[[i]])) {
+        plot0(xlim, ylim)
+        mtext(regions[i], side = 3, line = -1, at = 40, cex = .7*cex.txt)
+        par(mgp = c(1, 0.5, 0))
+        axis(1, lwd = 0, lwd.ticks = .5, mgp = c(2, .2, 0))
+        axis(2, lwd = 0, lwd.ticks = .5, mgp = c(2, .5, 0))
+        for (j in seq_len(nrow(ls_tre[[i]]))) {
             if (ls_tre[[i]]$disp[j]) {
                 id <- as.numeric(ls_tre[[i]]$tree[j])
                 if (ls_tre[[i]]$disp[j] > 2) {
                   seqy <- kern_exponential_power(seqd, scal = ls_tre[[i]]$scal[j],
                     shap = ls_tre[[i]]$shap[j])
                 } else {
-                  seqy <- kern_lognormal(seqd, scal = ls_tre[[i]]$scal[j], shap = ls_tre[[i]]$shap[j])
+                  seqy <- kern_lognormal(seqd, scal = ls_tre[[i]]$scal[j],
+                    shap = ls_tre[[i]]$shap[j])
                 }
-                lines(seqd, seqy, col = pal[id], lty = lty[id], lwd = 1.4)
+                lines(seqd, seqy, col = pal[id], lty = lty[id], lwd = 1.1)
             }
         }
-        # text(percX(80), percY(92), paste0("Age: ", 2 - i%%2), cex = 1.6)
         box(bty = "l")
     }
     ##
-    par(mar = c(1.5, 0, 1, 0))
+    par(mar = c(1, 0, .1, 0))
     plot0()
-    text(-0.2, 0, labels = "Density", srt = 90, cex = cex.txt)
+    text(-0.3, 0, labels = "density", srt = 90, cex = cex.txt)
+    text(rep(0.5, 2), c(-.57,.57), labels = c(2016, 2015), srt = 90, cex = .8*cex.txt)
     ##
-    par(mar = c(0.1, 2, 0, 1), xaxs = "i")
-    plot0(c(0, 3), c(0, 1))
-    text(c(0.12, 1.17, 2.24), rep(0.5, 3), labels = c("Aitibi", "Bic", "Sutton"), cex = cex.txt)
+    # par(mar = c(0.1, 2, 0, 1), xaxs = "i")
+    # plot0(c(0, 3), c(0, 1))
+    # text(c(0.12, 1.17, 2.24), rep(0.5, 3), labels = c("Aitibi", "Bic", "Sutton"), cex = cex.txt)
     ##
     plot0()
-    text(0, 0.7, labels = "Distance (m)", cex = cex.txt)
+    text(0, 0.72, labels = "Distance (m)", cex = cex.txt)
     ##
-    legend("bottom", bty = "n", legend = nm_tre, lwd = 2.4, lty = lty, seg.len = 2,
-        col = pal, ncol = 4, cex = 1.8)
+    legend("bottom", bty = "n", legend = nm_tre, lwd = 1.4, lty = lty,
+      seg.len = 2.7, col = pal, ncol = 7, cex = .7)
     ##
     invisible(NULL)
 }
+
+# png("fig5.png", width = 120, height = 80, units = "mm", res = 600)
+#  figKernels(res_bestModels)
+# dev.off()
+# png("figSI_age2.png", width = 120, height = 80, units = "mm", res = 600)
+#  figKernels(res_bestModels, age = 2)
+# dev.off()
